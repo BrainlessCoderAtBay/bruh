1f7d2304e6de7bd7fba53dfc8128148751cbcfa1
Rafi <sahriarrollins@gmail.com>
2025-12-15 11:10:13 -0500

    br

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-let myArray = [34, 2, 23, 67, 100, 5];
-let myStringArray = ["banana", "apple", "orange", "mango", "grape"];
-
-
 function arraySort(arr){
-    // Check if the input is an array
-    // Sort the array in natural sorting order
-    // Should show 2 different sorting methods
-    // dont use return
+    // Check if the input is an array and sort the array in natural sorting order
+    // Should show 2 different sorting methods and dont use return
     if (!Array.isArray(arr))console.log("Input is not an array");
 
     if (typeof arr[0] === "number"){ // Numeric sort
@@ -15,9 +9,3 @@ function arraySort(arr){
         arr.sort();
     }
 }
-
-
-arraySort(myArray);
-console.log(myArray);
-arraySort(myStringArray);
-console.log(myStringArray);
