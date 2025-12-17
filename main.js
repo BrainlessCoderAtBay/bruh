e97b6a7589a8c6b9fa9f6028fed594b3a535616f
Rafi <sahriarrollins@gmail.com>
2025-12-17 08:49:38 -0500

    fixed sort method

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
+
 function arraySort(arr){
     // Check if the input is an array and sort the array in natural sorting order
     // Should show 2 different sorting methods and dont use return
+    // Cant use arr.sort(). Manually_implement sorting logic
     if (!Array.isArray(arr))console.log("Input is not an array");
 
     if (typeof arr[0] === "number"){ // Numeric sort
-        arr.sort((a,b) => a-b);
+        for (let i = 0; i < arr.length - 1; i++){
+            for (let j = 0; j < arr.length - i - 1; j++){
+                if (arr[j] > arr[j + 1]){
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }else{ //String sort
-        arr.sort();
+        for (let i = 0; i < arr.length - 1; i++){
+            for (let j = 0; j < arr.length - i - 1; j++){
+                if (arr[j].localeCompare(arr[j + 1]) > 0){
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
